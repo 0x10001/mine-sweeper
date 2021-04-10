19d61ac3eca681de76f03060c6f996219d7f0e0d
Eric Wong <ericwong@zju.edu.cn>
2021-04-10 20:30:04 +0800

    test random

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import '../css/style.css'
+import { randRange } from './Random'
+
+for (let i = 0; i < 10; i++) {
+  console.log(randRange(10))
+}
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
