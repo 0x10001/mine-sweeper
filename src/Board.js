69f277aa8863f2a9f384b3763d8dd8b472b4e16d
Eric Wong <ericwong@zju.edu.cn>
2021-05-01 19:57:39 +0800

    add logics of game start

--- src/Board.js
+++ src/Board.js
@@ -1,3 +1,5 @@
+import { randRange } from "./Random"
+
 export class Board {
   // for debug
   debug() {
@@ -52,6 +54,30 @@ export class Board {
     this.#secret.fill(0, 0, this.#size)
   }
 
+  start(r, c) {
+    const neighbors = this.neighbors(r, c)
+    neighbors.push([r, c])
+
+    const mineSet = new Set(Array.from({length: this.#mines}, (_, i) => i))
+
+    for (let i = this.#size - neighbors.length; i > 1; i--) {
+      const j = randRange(i)
+      this.#swap(i - 1, j, mineSet)
+    }
+
+    let k = this.#size
+    for (const [y, x] of neighbors) {
+      this.#swap(this.#pack(y, x), --k, mineSet)
+    }
+
+    for (const idx of mineSet) {
+      const [sr, sc] = this.#unpack(idx)
+      for (const [y, x] of this.neighbors(sr, sc)) {
+        this.#board[y][x] & Board.#MASK ^ Board.#MINE && this.#board[y][x]++
+      }
+    }
+  }
+
   neighbors(r, c) {
     return Board.#neighbors.map(([dr, dc]) => [r + dr, c + dc]).filter(([y, x]) => this.#isInBoard(y, x))
   }
@@ -74,5 +100,30 @@ export class Board {
 
   #isInBoard(r, c) {
     return Board.#isInRange(c, this.#width) && Board.#isInRange(r, this.#height)
+  }
+
+  #pack(r, c) {
+    return r * this.#width + c
+  }
+
+  #unpack(index) {
+    const c = index % this.#width
+    const r = (index - c) / this.#width
+    return [r, c]
+  }
+
+  #swap(x, y, s) {
+    if ((this.#secret[x] ^ this.#secret[y]) & Board.#MASK) {
+      this.#secret[x] ^= this.#secret[y] & Board.#MASK
+      this.#secret[y] ^= this.#secret[x] & Board.#MASK
+      this.#secret[x] ^= this.#secret[y] & Board.#MASK
+      if (this.#secret[x] & Board.#MASK) {
+        s.delete(y)
+        s.add(x)
+      } else {
+        s.delete(x)
+        s.add(y)
+      }
+    }
   }
 }
