5c0738331111aff82b53cf740dfba5c641783dec
Eric Wong <ericwong@zju.edu.cn>
2021-05-08 19:54:37 +0800

    test logics of uncovering blocks

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ const secret = new Board()
 secret.start(5, 5)
 secret.debug()
 
+globalThis.uncover = function (r, c) {
+  console.clear()
+  secret.uncover(r, c)
+  secret.debug()
+}
+
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
